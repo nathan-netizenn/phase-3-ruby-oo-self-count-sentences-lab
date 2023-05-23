be141c17f759b0068b3e54c3a3a3f30f3008bb4e
nathan <nathanmageka6@gmail.com>
2023-05-23 22:52:31 +0300

    nathan

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,22 @@
-require 'pry'
-
 class String
 
   def sentence?
-    
+
+    end_with?('.')
   end
 
   def question?
 
+    end_with?('?')
   end
 
   def exclamation?
 
+    end_with?('!')
   end
 
   def count_sentences
 
+    split(/[.?!]+/).count
   end
 end
